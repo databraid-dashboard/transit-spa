8ec38b4816c88ebee419acf3e260d56cd4a343f3
Michael Murray <michaelmurray6298@gmail.com>
2017-08-09 08:53:32 -0700

    Fix CSS

--- libs/widget-libs/lib/App.js
+++ libs/widget-libs/lib/App.js
@@ -1,5 +1,5 @@
-var _jsxFileName = '../../src/App.jsx',
-    _this = this;
+let _jsxFileName = '../../src/App.jsx',
+  _this = this;
 
 import React from 'react';
 import { Grid, Container, Segment } from 'semantic-ui-react';
@@ -7,75 +7,74 @@ import DestinationTable from './components/Destinations/DestinationTable';
 import AddNewDestinationRow from './components/AddNewDestination/AddNewDestinationRow';
 import './App.css';
 
-var App = function App() {
+const App = function App() {
   return React.createElement(
     'div',
-<<<<<<< HEAD
     { className: 'transit-container', __source: {
-=======
-    {
-      __source: {
->>>>>>> Inital dashboard integration
         fileName: _jsxFileName,
-        lineNumber: 8
+        lineNumber: 8,
       },
-      __self: _this
+      __self: _this,
     },
     React.createElement(
       Container,
-<<<<<<< HEAD
-      { className: 'transit-container', __source: {
-=======
       {
         __source: {
->>>>>>> Inital dashboard integration
           fileName: _jsxFileName,
-          lineNumber: 9
+          lineNumber: 9,
         },
-        __self: _this
+        __self: _this,
       },
       React.createElement(
         Grid,
 <<<<<<< HEAD
         { className: 'transit-container', padded: true, centered: true, __source: {
 =======
-        { padded: true, centered: true, __source: {
->>>>>>> Inital dashboard integration
+        {
+          padded: true,
+          centered: true,
+          __source: {
+>>>>>>> Fix CSS
             fileName: _jsxFileName,
-            lineNumber: 10
+            lineNumber: 10,
           },
-          __self: _this
+          __self: _this,
         },
         React.createElement(
           Grid.Column,
 <<<<<<< HEAD
           { className: 'transit-container', __source: {
 =======
-          { id: 'content-container', __source: {
->>>>>>> Inital dashboard integration
+          {
+            id: 'content-container',
+            __source: {
+>>>>>>> Fix CSS
               fileName: _jsxFileName,
-              lineNumber: 11
+              lineNumber: 11,
             },
-            __self: _this
+            __self: _this,
           },
           React.createElement(
             Grid.Row,
-            { id: 'destinations-container', __source: {
+            {
+              id: 'destinations-container',
+              __source: {
                 fileName: _jsxFileName,
-                lineNumber: 12
+                lineNumber: 12,
               },
-              __self: _this
+              __self: _this,
             },
             React.createElement(DestinationTable, {
               __source: {
                 fileName: _jsxFileName,
-                lineNumber: 13
+                lineNumber: 13,
               },
-              __self: _this
-            })
+              __self: _this,
+            }),
           ),
           React.createElement(
             Grid.Row,
+<<<<<<< HEAD
 <<<<<<< HEAD
             {
               className: 'transit-container',
@@ -84,13 +83,19 @@ var App = function App() {
 =======
             { id: 'new-destination-container', __source: {
 >>>>>>> Inital dashboard integration
+=======
+            {
+              id: 'new-destination-container',
+              __source: {
+>>>>>>> Fix CSS
                 fileName: _jsxFileName,
-                lineNumber: 16
+                lineNumber: 16,
               },
-              __self: _this
+              __self: _this,
             },
             React.createElement(
               Segment,
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
               { className: 'transit-container', textAlign: 'center', __source: {
@@ -105,25 +110,36 @@ var App = function App() {
                   fileName: _jsxFileName,
                   lineNumber: 17
 >>>>>>> Inital dashboard integration
+=======
+              {
+                textAlign: 'center',
+                __source: {
+                  fileName: _jsxFileName,
+                  lineNumber: 17,
+>>>>>>> Fix CSS
                 },
-                __self: _this
+                __self: _this,
               },
               React.createElement(AddNewDestinationRow, {
                 __source: {
                   fileName: _jsxFileName,
 <<<<<<< HEAD
+<<<<<<< HEAD
                   lineNumber: 21
 =======
                   lineNumber: 18
 >>>>>>> Inital dashboard integration
+=======
+                  lineNumber: 18,
+>>>>>>> Fix CSS
                 },
-                __self: _this
-              })
-            )
-          )
-        )
-      )
-    )
+                __self: _this,
+              }),
+            ),
+          ),
+        ),
+      ),
+    ),
   );
 };
 
